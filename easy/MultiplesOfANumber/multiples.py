d5242fe51ce7b5d90329ca91279129b58d30a722
David Collins <davidcollins4481@gmail.com>
2013-06-17 20:13:46 -0400

    meh...files

---
+++ easy/MultiplesOfANumber/multiples.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# given numbers x and n, where n is a power of 2, print out the smallest multiple of n
+# which is greater than or equal to x. Do not use division or modulo operator
+# example input from file:
+# 13,8
+# 17,16
+# output sample:
+# 16
+# 32
+
